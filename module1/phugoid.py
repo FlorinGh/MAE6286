4c70de489717af6093cec99c8b6d33b4c97c5842
Florin Gheorghe <florin_gheorghe@ymail.com>
2015-09-16 20:53:23 +0300

    Module1 - WIP

---
+++ module1/phugoid.py
@@ -0,0 +1,20 @@
+#This script plots the flight path of the phugoid using the Lanchester's model
+
+import numpy as np
+import pylab as pl
+
+def radius_of_curvature(z, zt, C):
+    """ Returns the radius of curvature of the flight path at a given point
+    
+    Parameters
+    z (float): current depth below the reference horizontal line
+    zt (float): initial depth below the reference horizontal line
+    C (float): constant of integration
+    
+    
+    
+    """
+
+
+
+
